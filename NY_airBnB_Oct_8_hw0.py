1351b84d09dc69fcf81e92cdaac2f581b29b9733
jonghyockkim <56325890+jonghyockkim@users.noreply.github.com>
2019-10-13 17:50:46 -0400

    Update NY_airBnB_Oct_8_hw0.py
    New changes have made for "escaped quote" and "multiple quote"

--- NY_airBnB_Oct_8_hw0.py
+++ NY_airBnB_Oct_8_hw0.py
@@ -7,15 +7,16 @@ for line in lines[1:]:
     clean_columns = [] # this will contain the correctly-parsed columns
     in_quote = False  
     for x in columns: # we go through the "dirty" columns of data. We will set in_quote to True when we are inside a quote
+        clean_x = x.replace('""', '') # we remove the "escaped" quotes from x
+        n_quotes = clean_x.count('"')
         if not in_quote:
-            clean_columns += [ x ] # we aren't in a quote, we can add the column to the clean ones
-            if '"' in x:  # x contains a quote, we will have to handle the next columns differently
+            clean_columns += [ clean_x ] # we aren't in a quote, we can add the column to the clean ones
+            if n_quotes % 2 != 0:  # x contains an odd number of quotes, we will have to handle the next columns differently
                 in_quote = True
         else:
-            clean_columns[-1] += (','+ x)  # we are in a quote, we musn't create a new column. Instead, we append the current column to the last one in columns_clean
-            if '"' in x:  # this is our closing quote, next column will be out of the quote
+            clean_columns[-1] += clean_x  # we are in a quote, we musn't create a new column. Instead, we append the current column to the last one in columns_clean
+            if n_quotes % 2 != 0:  # this is our closing quote, next column will be out of the quote
                 in_quote = False
-    columns = line.split(',')
 
     try:  
         price = int(columns[9])
